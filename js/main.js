d87b055f9f56081f9b20378ddfb7fa0c6d5c9598
RahulNathani49 <nathanirahul49@gmail.com>
2019-08-11 04:48:38 -0400

    atintell

--- js/main.js
+++ js/main.js
@@ -40,21 +40,99 @@ var secondScene2 = new ScrollMagic.Scene({
     .setTween(mytween2)
     .addTo(controller2);
 
+var rule = CSSRulePlugin.getRule(".FirstMain::before");
 
-var blurtween = new TweenMax.to('body::before', 1, {
-    css: {
-        opacity: "1"
-    }
+var rule2 = CSSRulePlugin.getRule(".sec1::before");
+
+var blurtween = new TweenMax.to(rule, 3, {
+    opacity: "0",
+    delay: "4"
+});
+var imgtween = new TweenMax.to(".altlog", 2, {
+    opacity: "1",
+    delay: "1"
+});
+var bordtween = new TweenMax.to(".bord", 2, {
+    opacity: "1",
+    delay: "2"
+});
+var bordtween = new TweenMax.to(".back", .5, {
+    opacity: "1",
+    delay: "3",
+    transform: "translateY(0)"
+});
+var dashtween = new TweenMax.to(".dash", .5, {
+    opacity: "1",
+    delay: "3",
+    transform: "translateY(0)"
+});
+
+var blurtween2 = new TweenMax.to(rule, 3, {
+    opacity: "1"
 });
 
 var blurcontroller = new ScrollMagic.Controller();
 var blurscene = new ScrollMagic.Scene({
         triggerElement: '.FirstMain',
-        triggerHook: "0",
-        duration: "400px"
+        duration: "400px",
+        offset: "500px"
+
     })
     .addIndicators({
         name: "startblur"
     })
-    .setTween(blurtween)
-    .addTo(blurcontroller);
+    .setTween(blurtween2)
+    .addTo(blurcontroller)
+
+
+
+var blurtween3 = new TweenMax.to(rule2, 3, {
+    opacity: "1"
+});
+
+var blurcontroller2 = new ScrollMagic.Controller();
+var blurscene = new ScrollMagic.Scene({
+        triggerElement: '.FirstMain',
+        duration: "400px",
+        offset: "500px"
+
+    })
+    .addIndicators({
+        name: "startblur"
+    })
+    .setTween(blurtween3)
+    .addTo(blurcontroller2)
+
+
+
+var numbercontroller = new ScrollMagic.Controller();
+var scene = new ScrollMagic.Scene({
+        triggerElement: "#sec1",
+        triggerHook: '1',
+        duration: "550px"
+    })
+    .on('progress', function() {
+        $('#number').text(Math.floor((Math.random() * 15) + 1));
+    })
+    .on("leave", function(event) {
+        $('#number').text("15");
+    })
+    .addTo(numbercontroller)
+    .addIndicators({ name: "number" });
+
+
+
+var testimonaltween = new TweenMax.to('.testimonal', .8, {
+    css: {
+        transform: "translateX(40%)",
+        opacity: "1"
+    }
+});
+var testcontroller = new ScrollMagic.Controller();
+var secondScene = new ScrollMagic.Scene({
+        triggerElement: '.content',
+        offset: "50px"
+    })
+    .addIndicators()
+    .setTween(testimonaltween)
+    .addTo(testcontroller);
